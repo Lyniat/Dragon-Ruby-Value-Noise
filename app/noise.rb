e6ea4b2393ff3e41e9c1d77099c2d494b5800ea2
Laurin Muth <l.muth@lyniat.games>
2022-02-09 15:24:50 +0100

    Update noise.rb

--- app/noise.rb
+++ app/noise.rb
@@ -15,9 +15,9 @@ class Noise
     a = 0.5
     (0...@octaves).each do |i|
       v += a * noise(pos_x, pos_y)
-      result = rotate(pos_x * 2, pos_y * 2)
-      pos_x = result.x + 100
-      pos_y = result.y + 100
+      r_x, r_y = rotate(pos_x * 2, pos_y * 2)
+      pos_x = r_x + 100
+      pos_y = r_y + 100
       a *= 0.5
     end
     v
@@ -26,40 +26,41 @@ class Noise
   private
 
   def noise(x, y)
-    st = {x: x, y: y}
-    i = {x: st.x.floor, y: st.y.floor}
-    f = {x: fract(st.x), y: fract(st.y)}
+    i_x = x.floor
+    i_y = y.floor
+    f_x = fract(x)
+    f_y = fract(y)
 
     # Four corners in 2D of a tile
-    vec_b = {x: 1 + i.x, y: i.y}
-    vec_c = {x: i.x,y: i.y + 1}
-    vec_d = {x: i.x + 1,y: i.y + 1}
-    a = hash(i)
-    b = hash(vec_b)
-    c = hash(vec_c)
-    d = hash(vec_d)
+    a = hash(i_x,i_y)
+    b = hash(i_x + 1, i_y)
+    c = hash(i_x, i_y + 1)
+    d = hash(i_x + 1, i_y + 1)
 
     # Smooth Interpolation
     # Cubic Hermine Curve
-    u = {x: f.x**2 * (3 - 2 * f.x),y: f.y**2 * (3 - 2 * f.y)}
+    u_x = f_x**2 * (3 - 2 * f_x)
+    u_y = f_y**2 * (3 - 2 * f_y)
 
     # Mix 4 corners percentages
-    mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y
+    mix(a, b, u_x) + (c - a) * u_y * (1.0 - u_x) + (d - b) * u_x * u_y
   end
 
   def rotate(x, y)
-    {x: COS * x + SIN * y, y: -SIN * x + COS * y}
+    [COS * x + SIN * y, -SIN * x + COS * y]
   end
 
-  def hash(p)
-    p3 = {x: fract(p.x * 0.13 * @seed),y: fract(p.y * 0.13),z: fract(p.x * 0.13)}
-    d = vec3_dot(p3, {x: p3.y + 3.333,y: p3.z + 3.333,z: p3.x + 3.333})
-    q3 = {x: p3.x + d,y: p3.y + d,z: p3.z + d}
-    fract((q3.x + q3.y) * q3.z)
+  def hash(x, y)
+    p3_xz = fract(x * 0.13 * @seed)
+    p3_y = fract(y * 0.13)
+    d = vec3_dot(p3_xz,p3_y,p3_xz, p3_y + 3.333,p3_xz + 3.333, p3_xz + 3.333)
+    q3_xz = p3_xz + d
+    q3_y = p3_y + d
+    fract((q3_xz + q3_y) * q3_xz)
   end
 
-  def vec3_dot vec_a, vec_b
-    vec_a.x * vec_b.x + vec_a.y * vec_b.y + vec_a.z * vec_b.z
+  def vec3_dot (ax, ay, az, bx ,by, bz)
+    ax * bx + ay * by + az * bz
   end
 
   def fract(num)

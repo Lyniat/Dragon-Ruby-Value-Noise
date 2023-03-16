76dcadcb47a688813d9efd025036906461794c37
Laurin Muth <l.muth@lyniat.games>
2023-03-16 10:43:04 +0100

    Add cellular noise

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# DragonRuby Value Noise
-A implementation of value noise for Dragon Ruby.
-The noise function works with standard DR but the example needs DR *Indie* or *Pro*.
+# DragonRuby Noise Library
+A implementation of different noise functions for Dragon Ruby.
+The noise functions work with standard DR but the example needs currently DR *Indie* or *Pro*.
+Examples for DR standard are WIP.
+If you want to report bugs, have improvements or want in generally talk about this project, visit [the official DragonRuby Discord](http://discord.dragonruby.org) and mention *@lyniat*.
 
+# Examples
+## Value Noise
 ![Screenshot 1](screenshots/sc1.png)
 ![Screenshot 2](screenshots/sc2.png)
+
+## Cellular Noise
+![Screenshot Cellular 1](screenshots/sc_cellular_1.png)
+![Screenshot Cellular 0](screenshots/sc_cellular_0.png)
+
+# Performance
+While trying to keep things fast, performance isn't the main goal of this project but the ease of use.
+If you need some faster noise (and by faster I mean light speed like 🙃), I can totally recommend [Fast Noise Lite](https://github.com/Auburn/FastNoiseLite). (Keep in mind that *Fast Noise Lite* needs you to compile C/C++ code)
+
+# License
+See LICENSE file for more details.
+Some code is taken/ported from [Fast Noise Lite](https://github.com/Auburn/FastNoiseLite) ([license](https://github.com/Auburn/FastNoiseLite/blob/master/LICENSE)) and [1D, 2D & 3D Value Noise](https://www.shadertoy.com/view/4dS3Wd) (BSD license).
--- app/main.rb
+++ app/main.rb
@@ -3,33 +3,58 @@ require 'app/noise.rb'
 WIDTH = 1280
 HEIGHT = 720
 SIZE = 200
-SEED = 3
+SEED = 0
 SCALE = 0.07
 OFFSET = -SIZE / 2
-OCTAVES = 8
+OCTAVES = 4
 
-def init args
-  noise = Noise.new(SEED, OCTAVES)
-
+def generate_noise args
   args.pixel_array(:noise).width = SIZE
   args.pixel_array(:noise).height = SIZE
+
+  noise = Noise.new(SEED + args.state.iteration, OCTAVES)
 
   x = 0
   while x < SIZE
     y = 0
     while y < SIZE
-      n = noise.get((x + OFFSET) * SCALE, (y + OFFSET) * SCALE)
-      n = ((n + 1) * 127).floor
-      args.pixel_array(:noise).pixels[y * SIZE + x] = 0xFF000000 + n + (n << 8) + (n << 16)
+      case args.state.noise_mode
+      when 0
+        n = noise.get_fbm((x + OFFSET) * SCALE, (y + OFFSET) * SCALE)
+        n = ((n + 1) * 127).floor
+        args.pixel_array(:noise).pixels[y * SIZE + x] = 0xFF000000 + n + (n << 8) + (n << 16)
+      when 1
+        n = noise.get_cellular((x + OFFSET) * SCALE, (y + OFFSET) * SCALE)
+        n = (n / 4) * 255 * 255 * 255
+        args.pixel_array(:noise).pixels[y * SIZE + x] = 0xFF000000 + n.floor
+      end
       y += 1
     end
     x += 1
   end
 
+  args.state.iteration += 1
 end
 
 def tick args
-  init(args) if args.state.tick_count.zero?
+  args.state.iteration ||= 0
+  args.state.last_noise_mode ||= 0
+  args.state.noise_mode ||= args.state.last_noise_mode
 
-  args.outputs.primitives << [0, 0, WIDTH, HEIGHT, :noise].sprite
+  args.state.noise_mode += 1 if args.inputs.keyboard.key_down.space
+  args.state.noise_mode = 0 if args.state.noise_mode > 1
+
+  generate_noise(args) if args.state.tick_count.zero? || args.state.noise_mode != args.state.last_noise_mode
+
+  args.state.last_noise_mode = args.state.noise_mode
+
+  args.outputs.sprites << {
+    x: 0,
+    y: 0,
+    w: WIDTH,
+    h: HEIGHT,
+    path: :noise
+  }
+
+  args.outputs.labels << [0, HEIGHT, "PRESS SPACE TO CHANGE NOISE", 1, 0, 255, 0, 0]
 end
